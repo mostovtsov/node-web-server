f27a4304f9afa78ee9ee933fbced5b29e49d1167
IIIypik <mostovtsov@gmail.com>
2016-12-06 21:02:47 +0300

    just for push

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const hbs = require('hbs');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 
-
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-
 
 app.use((req, res, next) => {
     var now = new Date().toString();
